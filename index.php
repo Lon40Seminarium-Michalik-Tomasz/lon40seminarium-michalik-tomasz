6ecb570fdfde86ceba8bd9e99743797911fa1335
lon40seminariumMichalikTomasz <tomaszmichaliklon40@gmail.com>
2026-02-12 16:12:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
         <div id="fig1">
         <fieldset id="f1">
         <legend>Prostokąt</legend>
-        <label for="pr_a">Długość boku a </label></br>
+        <label for="pr_a">Długość boku a = </label></br>
         <input type="text" id="pr_a" name="pr_a"/></br>
-        <label for="pr b">Długość boku b</label></br> 
+        <label for="pr_b">Długość boku b =</label></br> 
         <input type="text" id="pr_b" name="pr_b"/></br>
         <input type="button" value="Oblicz" onclick="obliczPr();"/>
 
